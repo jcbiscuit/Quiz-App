b1251381afc776838f971808ea1f509faed631c9
Jody Mazinyi <36807329+jcbiscuit@users.noreply.github.com>
2020-11-23 15:42:03 +1100

    Add files via upload

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Jody Clelland on 13/10/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+
+
+init(q: String, a: String) {
+    text = q
+    answer = a
+}
+}
